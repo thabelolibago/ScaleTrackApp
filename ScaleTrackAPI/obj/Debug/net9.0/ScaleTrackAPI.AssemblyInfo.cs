853e83ff25e180a5ab97e930a8933f95a7093b84
thabelolibago <thabelolibago@gmail.com>
2025-10-27 15:17:43 +0200

    []

--- ScaleTrackAPI/obj/Debug/net9.0/ScaleTrackAPI.AssemblyInfo.cs
+++ ScaleTrackAPI/obj/Debug/net9.0/ScaleTrackAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScaleTrackAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89a54b11f3ac61f242f3e03b67e8c6fd0fbd873a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77a050136ef8501fcf5c53175174e7e5376e1360")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScaleTrackAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScaleTrackAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
