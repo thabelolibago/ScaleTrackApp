168e156fcf7b843535ea7bb2c949c80735ce640a
thabelolibago <thabelolibago@gmail.com>
2025-11-14 09:46:45 +0200

    Added auditTrail to the Verify Email

--- ScaleTrackAPI/Application/Features/Auth/VerifyEmail/BusinessRules/VerifyEmailBusinessRules.cs
+++ ScaleTrackAPI/Application/Features/Auth/VerifyEmail/BusinessRules/VerifyEmailBusinessRules.cs
@@ -1,22 +1,24 @@
 using ScaleTrackAPI.Application.Errors.ErrorMessages;
 using ScaleTrackAPI.Application.Messages.SuccessMessages;
 using ScaleTrackAPI.Infrastructure.Repositories.Interfaces.IUserRepository;
+using ScaleTrackAPI.Domain.Entities;
 
 namespace ScaleTrackAPI.Application.Features.Auth.VerifyEmail.BusinessRules
 {
     public class VerifyEmailBusinessRules
     {
         private readonly IUserRepository _userRepo;
+
         public VerifyEmailBusinessRules(IUserRepository userRepo)
         {
             _userRepo = userRepo;
         }
-  
-        public async Task<(bool Success, string Message)> VerifyEmailAsync(string token)
+
+        public async Task<(bool Success, string Message, User? User)> VerifyEmailAsync(string token)
         {
             var user = await _userRepo.GetByVerificationToken(token);
             if (user == null)
-                return (false, ErrorMessages.Get("Auth:InvalidToken"));
+                return (false, ErrorMessages.Get("Auth:InvalidToken"), null);
 
             user.IsEmailVerified = true;
             user.RequiresEmailVerification = false;
@@ -25,7 +27,7 @@ namespace ScaleTrackAPI.Application.Features.Auth.VerifyEmail.BusinessRules
 
             await _userRepo.Update(user);
 
-            return (true, SuccessMessages.Get("Email:EmailVerified"));
+            return (true, SuccessMessages.Get("Email:EmailVerified"), user);
         }
     }
 }
